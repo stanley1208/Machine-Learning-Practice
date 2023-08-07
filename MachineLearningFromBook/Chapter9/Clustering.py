a3ddbf90f3d6bca52ade6b26fed8242e8658c189
A8250092 <stanley.mei08@gmail.com>
2023-08-07 14:09:38 +0800

    Update Clustering.py

--- MachineLearningFromBook/Chapter9/Clustering.py
+++ MachineLearningFromBook/Chapter9/Clustering.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 from scipy import stats
 import numpy as np
+from sklearn.datasets import make_blobs
+
 
 data=load_iris()
 X=data.data
@@ -42,13 +44,32 @@ print(mapping)
 
 y_pred=np.array([mapping[cluster_id] for cluster_id in y_pred])
 
-plt.plot(X[y_pred==0,2],X[y_pred==0,3],'yo',label='Cluster1')
-plt.plot(X[y_pred==1,2],X[y_pred==1,3],'bs',label='Cluster2')
-plt.plot(X[y_pred==2,2],X[y_pred==2,3],'g^',label='Cluster3')
-plt.xlabel("Patel length",fontsize=18)
-plt.ylabel("Patel width",fontsize=18)
-plt.legend(loc='best',fontsize=18)
-plt.show()
+# plt.plot(X[y_pred==0,2],X[y_pred==0,3],'yo',label='Cluster1')
+# plt.plot(X[y_pred==1,2],X[y_pred==1,3],'bs',label='Cluster2')
+# plt.plot(X[y_pred==2,2],X[y_pred==2,3],'g^',label='Cluster3')
+# plt.xlabel("Patel length",fontsize=18)
+# plt.ylabel("Patel width",fontsize=18)
+# plt.legend(loc='best',fontsize=18)
+# plt.show()
 
 print(np.sum(y_pred==y))
 print(np.sum(y_pred==y)/len(y_pred))
+
+blob_centers=np.array(
+    [[0.2,2.3],
+     [-1.5,2.3],
+     [-2.8,1.8],
+     [-2.8,2.8],
+     [-2.8,1.3]]
+)
+blob_std=np.array([0.4,0.3,0.1,0.1,0.1])
+X,y=make_blobs(n_samples=2000,centers=blob_centers,cluster_std=blob_std,random_state=7)
+
+def plot_cluster(X,y=None):
+    plt.scatter(X[:,0],X[:,1],c=y,s=1)
+    plt.xlabel("$x_1$",fontsize=14)
+    plt.ylabel("$y_1$",fontsize=14,rotation=0)
+
+plt.figure(figsize=(10,8))
+plot_cluster(X)
+plt.show()

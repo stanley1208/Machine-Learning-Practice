e3f5ca81f92b33d56f57658014a587ff23ea7c91
A8250092 <stanley.mei08@gmail.com>
2023-08-14 16:09:31 +0800

    Update Clustering.py

--- MachineLearningFromBook/Chapter9/Clustering.py
+++ MachineLearningFromBook/Chapter9/Clustering.py
@@ -13,6 +13,41 @@ y=data.target
 print(data.target_names)
 
 
+def plot_data(X):
+    plt.plot(X[:,0],X[:,1],'k.',markersize=2)
+
+def plot_centroids(centroids,weights=None,circle_color='w',cross_color='k'):
+    if weights is not None:
+        centroids=centroids[weights>weights.max()/10]
+    plt.scatter(centroids[:,0],centroids[:,1],marker='o',s=35,linewidth=8,color=circle_color,zorder=10,alpha=0.9)
+    plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=2,linewidth=12,color=cross_color,zorder=11,alpha=1)
+
+def plot_decision_boundaries(clusterer,X,resolution=10000,show_centroids=True,show_xlabels=True,show_ylabels=True):
+    mins=X.min(axis=0)-0.1
+    maxs=X.max(axis=0)+0.1
+    xx,yy=np.meshgrid(np.linspace(mins[0],maxs[0],resolution),
+                      np.linspace(mins[1], maxs[1], resolution))
+    Z=clusterer.predict(np.c_[xx.ravel(),yy.ravel()])
+    Z=Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                 cmap="Pastel2")
+    plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                linewidths=1, colors='k')
+    plot_data(X)
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+
+
 # plt.figure(figsize=(10,8))
 #
 # plt.subplot(121)
@@ -87,3 +122,8 @@ print(kmeans.labels_)
 
 X_new=np.array([[0,2],[3,2],[-3,3],[-3,2.5]])
 print(kmeans.predict(X_new))
+
+
+plt.figure(figsize=(10,8))
+plot_decision_boundaries(kmeans,X)
+plt.show()

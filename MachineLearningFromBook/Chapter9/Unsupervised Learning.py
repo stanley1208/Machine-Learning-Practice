27935927b5613a9735db40740d85faf6476e5cbd
stanley1208 <stanley.mei08@gmail.com>
2024-08-12 10:43:33 +0800

    Update Unsupervised Learning.py

--- MachineLearningFromBook/Chapter9/Unsupervised Learning.py
+++ MachineLearningFromBook/Chapter9/Unsupervised Learning.py
@@ -109,3 +109,23 @@ def plot_centroids(centroids,weight=None,circle_color='w',cross_color='k'):
     plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=2, linewidths=12, color=cross_color, zorder=11,
                 alpha=1)
 
+
+def plot_decision_boundaries(clusterer,X,resolution=1000,show_centriods=True,show_xlabels=True,show_ylabels=True):
+    mins=X.min(axis=0)-0.1
+    maxs=X.max(axis=0)+0.1
+    xx,yy=np.meshgrid(np.linspace(mins[0],maxs[0],resolution),np.linspace(mins[1],maxs[1],resolution))
+    Z=clusterer.predict(np.c_[xx.ravel(),yy.ravel()])
+    Z=Z.reshape(xx.shape)
+    plt.contourf(Z,extent=(mins[0],maxs[0],mins[1],maxs[1]),cmap="Pastel2")
+    plt.contour(Z,extent=(mins[0],maxs[0],mins[1],maxs[1]),linewidths=1,color='k')
+    plot_data(X)
+    if show_centriods:
+        plot_centroids(clusterer.cluster_centers_)
+    if show_xlabels:
+        plt.xlabel("x_1")
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("x_2",rotation=0)
+    else:
+        plt.tick_params(labelleft=False)

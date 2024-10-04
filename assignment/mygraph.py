cae4995c4566829120480d15a89d099bc15a1900
stanley1208 <stanley.mei08@gmail.com>
2024-10-03 23:53:12 -0700

    update

--- assignment/mygraph.py
+++ assignment/mygraph.py
@@ -1,26 +1,48 @@
-import csv
+edges = [
+    ("Arad", "Zerind", 75),
+    ("Arad", "Sibiu", 140),
+    ("Arad", "Timisoara", 118),
+    ("Bucharest", "Pitesti", 101),
+    ("Bucharest", "Urziceni", 85),
+    ("Bucharest", "Giurglu", 90),
+    ("Craiova", "Dobreta", 120),
+    ("Fagaras", "Bucharest", 211),
+    ("Hirsova", "Eforie", 86),
+    ("Lasi", "Neamt", 87),
+    ("Lugoj", "Mehadia", 70),
+    ("Mehadia", "Dobreta", 75),
+    ("Oradea", "Sibiu", 151),
+    ("Pitesti", "Craiova", 138),
+    ("Rimnicu Vilcea", "Pitesti", 97),
+    ("Rimnicu Vilcea", "Craiova", 146),
+    ("Sibiu", "Fagaras", 99),
+    ("Sibiu", "Rimnicu Vilcea", 80),
+    ("Timisoara", "Lugoj", 111),
+    ("Urziceni", "Hirsova", 98),
+    ("Urziceni", "Vaslui", 142),
+    ("Vaslui", "Lasi", 92),
+    ("Zerind", "Oradea", 71)
+]
 
-graph={}
 
-with open('graph.txt','r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        c1,c2,cost=row
-        cost=int(cost)
+graph = {}
 
-        if c1 not in graph:
-            graph[c1]=[]
-        if c2 not in graph:
-            graph[c2]=[]
+for edge in edges:
+    node1, node2, weight = edge
 
-        graph[c1].append((c2,cost))
-        graph[c2].append((c1,cost))
+    if node1 not in graph:
+        graph[node1] = []
+    if node2 not in graph:
+        graph[node2] = []
 
-num=len(graph)
-edges=sum(len(edges) for edges in graph.values())//2
 
-print(num)
-print(edges)
-print(graph)
-print(graph.keys())
-print(graph.values())
+    graph[node1].append((node2, weight))
+    graph[node2].append((node1, weight))
+
+# Count vertices and edges
+nodes = len(graph)
+edges = len(edges)
+
+# Print results
+print("Number of vertices:", nodes)
+print("Number of edges:", edges)
